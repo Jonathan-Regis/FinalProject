3068a239c6e70ce29b382c10d7a59ce0ae8f7d89
Nicholas Zhang <zhan0790@algonquinlive.com>
2023-08-09 21:12:17 -0400

    android final got different currency array and value inputs on the activity currency

--- app/src/main/java/com/example/finalproject/TriviaActivity.java
+++ app/src/main/java/com/example/finalproject/TriviaActivity.java
@@ -3,6 +3,4 @@ package com.example.finalproject;
 import com.example.finalproject.databinding.ActivityTriviaBinding;
 
 public class TriviaActivity {
-    ActivityTriviaBinding binding = ActivityTriviaBinding.inflate();
-    setContentView(binding.getRoot());
 }
